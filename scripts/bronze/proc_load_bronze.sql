f7190fc39a34f43173563a6f7249c6dc7d4bc545
Jurgita Žilinskaitė <52500483+jurgita100@users.noreply.github.com>
2025-10-26 13:14:07 +0200

    Create proc_load_bronze.sql

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,18 @@
+/*Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.*/
+
+
+TRUNCATE TABLE bronze.crm_cust_info;
+\copy bronze.crm_cust_info FROM 'C:\Users\Lenovo\Desktop\SQL_\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+TRUNCATE TABLE bronze.crm_prd_info;
+\copy bronze.crm_prd_info FROM 'C:\Users\Lenovo\Desktop\SQL_\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+TRUNCATE TABLE bronze.crm_sales_details;
+\copy bronze.crm_sales_details FROM 'C:\Users\Lenovo\Desktop\SQL_\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+TRUNCATE TABLE bronze.erp_cust_az12;
+\copy bronze.erp_cust_az12 FROM 'C:\Users\Lenovo\Desktop\SQL_\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+TRUNCATE TABLE bronze.erp_loc_a101;
+\copy bronze.erp_loc_a101 FROM 'C:\Users\Lenovo\Desktop\SQL_\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+\copy bronze.erp_px_cat_g1v2 FROM 'C:\Users\Lenovo\Desktop\SQL_\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
