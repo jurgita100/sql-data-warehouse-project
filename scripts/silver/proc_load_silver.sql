57dccc0b87e454f8ee2a88e8ceb44c5db91e3aa5
Jurgita Žilinskaitė <52500483+jurgita100@users.noreply.github.com>
2025-11-21 19:59:32 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -128,3 +128,18 @@ SELECT
   END AS cntry
 FROM bronze.erp_loc_a101
 WHERE cid NOT IN (SELECT cst_key FROM silver.crm_cust_info);
+
+
+
+INSERT INTO silver.erp_px_cat_g1v2 (
+  id,
+  cat,
+  subcat,
+  maintenance
+)
+SELECT
+  id,
+  cat,
+  subcat,
+  maintenance
+FROM bronze.erp_px_cat_g1v2;
