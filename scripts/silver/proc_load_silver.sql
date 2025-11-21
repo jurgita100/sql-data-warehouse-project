91173149a1cb8a1d037c64682c34a8a9a23493e2
Jurgita Žilinskaitė <52500483+jurgita100@users.noreply.github.com>
2025-11-21 11:26:45 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -92,3 +92,23 @@ SELECT
        ELSE sls_price
   END AS sls_price
 FROM bronze.crm_sales_details;
+
+
+
+INSERt INTO silver.erp_cust_az12 (
+  cid,
+  bdate,
+  gen
+)
+SELECT
+  CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid))
+       ELSE cid
+  END AS cid,
+  CASE WHEN bdate > NOW() THEN NULL
+       ELSE bdate
+  END AS bdate,
+  CASE WHEN UPPER(TRIM(gen)) LIKE 'F%' THEN 'Female'
+       WHEN UPPER(TRIM(gen)) LIKE 'M%' THEN 'Male'
+       ELSE 'n/a'
+  END AS gen
+FROM bronze.erp_cust_az12;
